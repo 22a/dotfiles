d6920f65108814bdaa57a10ee2499871f30ad240
22a <meehanp2@tcd.ie>
2015-11-08 13:04:18 +0000

    generalised :battery: indicator script

--- bin/battery_indicator.sh
+++ bin/battery_indicator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # modified from http://ficate.com/blog/2012/10/15/battery-life-in-the-land-of-tmux/
 
-HEART='♥ '
+HEART=' ♥'
 
 if [[ `uname` == 'Linux'  ]]; then
   current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
@@ -12,15 +12,17 @@ else
   total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
-charged_slots=$(echo "((($current_charge/$total_charge)*10)/3)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 3  ]]; then
-  charged_slots=3
+breakpoints=10
+charged_slots=$(echo "((($current_charge/$total_charge)*100)/(100/$breakpoints))+1" | bc -l | cut -d '.' -f 1)
+
+if [[ $charged_slots -gt $breakpoints ]]; then
+  charged_slots=$breakpoints
 fi
 
 echo -n '#[fg=colour203]'
 for i in `seq 1 $charged_slots`; do echo -n "$HEART"; done
 
-if [[ $charged_slots -lt 3  ]]; then
+if [[ $charged_slots -lt $breakpoints ]]; then
   echo -n '#[fg=colour254]'
-  for i in `seq 1 $(echo "3-$charged_slots" | bc)`; do echo -n "$HEART"; done
+  for i in `seq 1 $(echo "$breakpoints-$charged_slots" | bc)`; do echo -n "$HEART"; done
 fi
