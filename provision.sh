0b2e3c7b0fec4b7166c4ac638b38d037d5c1266f
Peter Meehan <meehanp2@tcd.ie>
2018-03-25 03:57:22 +0100

    fleebo

--- provision.sh
+++ provision.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Ask for the administrator password upfront
-sudo -v
-
-# update existing `sudo` time stamp until `provision.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-sudo bash -x provision/brew.sh
-sudo bash -x provision/zsh.sh
+bash -x provision/brew.sh
+bash -x provision/zsh.sh
 bash -x provision/neovim.sh
 bash -x provision/symlinks.sh
 bash -x provision/macos.sh
