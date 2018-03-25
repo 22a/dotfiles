c060594604d234f777f8baa0e209293539870c73
Peter Meehan <meehanp2@tcd.ie>
2018-03-25 18:04:47 +0100

    eulogy

--- provision/zsh.sh
+++ provision/zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use brew zsh instead of bash
-sudo tee -a /etc/shells < '/usr/local/bin/zsh'
+echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
 
 # install zsh plugins
