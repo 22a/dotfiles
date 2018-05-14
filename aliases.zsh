e44882706d968f564aa0a272ac1e89fea130441d
Peter Meehan <peter.meehan@intercom.io>
2018-05-14 16:46:57 +0100

    noot noot

--- aliases.zsh
+++ aliases.zsh
@@ -96,6 +96,11 @@ alias la="ls -lA"
 alias ll="la"
 alias lk="la -S"
 
+# ripgrep
+alias rg="rg --smart-case"
+
 # dotfile editing
 alias nvimrc="$EDITOR ~/dotfiles/nvimrc"
 alias zshrc="$EDITOR ~/dotfiles/zshrc"
+
+alias s="script/server"
