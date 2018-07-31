6d42c939711a45f305dd8184536129a88bab23df
Peter Meehan <meehanp2@tcd.ie>
2018-08-01 00:51:07 +0100

    relocate provision scripts

---
+++ scripts/provision/all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+provision_dir=$(dirname "$0")
+
+bash -x "$provision_dir/brew.sh"
+bash -x "$provision_dir/brewfile.sh"
+bash -x "$provision_dir/macos.sh"
+bash -x "$provision_dir/zsh.sh"
+bash -x "$provision_dir/fzf.sh"
+bash -x "$provision_dir/neovim.sh"
+bash -x "$provision_dir/symlinks.sh"
+bash -x "$provision_dir/zplug.sh"
+bash -x "$provision_dir/vimplug.sh"
