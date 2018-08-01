4c0fca46e360249d3c75f7708756c7b074023565
Peter Meehan <meehanp2@tcd.ie>
2018-08-01 01:25:06 +0100

    clean up symlinks

--- scripts/provision/symlinks.sh
+++ scripts/provision/symlinks.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/git
-ln -sf $PWD/nvimrc ~/.config/nvim/init.vim
-ln -sf $PWD/zshrc ~/.zshrc
-ln -sf $PWD/zshenv ~/.zshenv
-ln -sf $PWD/zprofile ~/.zprofile
-ln -sf $PWD/aliases.zsh ~/.aliases
-ln -sf $PWD/tmux.conf ~/.tmux.conf
-ln -sf $PWD/gitconfig ~/.gitconfig
-ln -sf $PWD/gitignore ~/.gitignore
 touch ~/.hushlogin
 touch ~/.aliases.priv
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/git
+
+ln -sf "$PWD/nvimrc"      ~/.config/nvim/init.vim
+ln -sf "$PWD/zshrc"       ~/.zshrc
+ln -sf "$PWD/aliases.zsh" ~/.aliases
+ln -sf "$PWD/tmux.conf"   ~/.tmux.conf
+ln -sf "$PWD/gitconfig"   ~/.gitconfig
+ln -sf "$PWD/.gitignore"  ~/.gitignore
