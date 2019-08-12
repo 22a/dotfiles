0a70a86a9407613942a2bd274baef9a7a71dd36a
Peter Meehan <peter.meehan@intercom.io>
2019-08-12 15:54:01 +0100

    add ws alias

--- aliases.zsh
+++ aliases.zsh
@@ -131,3 +131,4 @@ alias ys="yarn install --check-files; rm -rf tmp dist; ember s"
 alias nif="git check-ignore * | tr '\\n' ' ' | xargs rm -rf"
 
 alias percy="perl -pi -e \"s/RUN_PERCY=''/RUN_PERCY='true'/g\" .env; git add .env; gcm \"run percy\"; git push; git revert HEAD --no-edit; git push;"
+alias ws="hammer workspace --vpn -u $(cat ~/.intercom_username)"
