4546f63fc2a002a64fdb2e85266945f5108ba20a
Peter Meehan <peter.meehan@intercom.io>
2023-04-12 20:48:50 +0100

    no more fzf completion / zplug

--- scripts/provision/symlinks.sh
+++ scripts/provision/symlinks.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.config/nvim
 
 ln -sf "$PWD/nvimrc"      ~/.config/nvim/init.vim
 ln -sf "$PWD/zshrc"       ~/.zshrc
-ln -sf "$PWD/zimrc"       ~/.config/.zimrc
+ln -sf "$PWD/zimrc"       ~/.zimrc
 ln -sf "$PWD/aliases.zsh" ~/.aliases
 ln -sf "$PWD/tmux.conf"   ~/.tmux.conf
 ln -sf "$PWD/gitconfig"   ~/.gitconfig
