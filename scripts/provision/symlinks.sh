d68a07912bae8e27925030aea544189b9342e07c
Peter Meehan <peter.meehan@intercom.io>
2023-04-12 22:13:26 +0100

    add astronvim user config

--- scripts/provision/symlinks.sh
+++ scripts/provision/symlinks.sh
@@ -2,11 +2,11 @@
 
 touch ~/.hushlogin
 touch ~/.aliases.priv
-mkdir -p ~/.config/nvim
+mkdir -p ~/.config/nvim/lua/user
 
-ln -sf "$PWD/nvimrc"      ~/.config/nvim/init.vim
-ln -sf "$PWD/zshrc"       ~/.zshrc
-ln -sf "$PWD/zimrc"       ~/.zimrc
-ln -sf "$PWD/aliases.zsh" ~/.aliases
-ln -sf "$PWD/tmux.conf"   ~/.tmux.conf
-ln -sf "$PWD/gitconfig"   ~/.gitconfig
+ln -sf "$PWD/zshrc"              ~/.zshrc
+ln -sf "$PWD/zimrc"              ~/.zimrc
+ln -sf "$PWD/aliases.zsh"        ~/.aliases
+ln -sf "$PWD/tmux.conf"          ~/.tmux.conf
+ln -sf "$PWD/gitconfig"          ~/.gitconfig
+ln -sf "$PWD/astronvim-init.lua" ~/.config/nvim/lua/user/init.lua
