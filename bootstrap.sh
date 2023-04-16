8fe93cd7b381bbb27bbfb9d01060a74326af7357
Peter Meehan <peter.meehan@intercom.io>
2023-04-16 19:31:21 +0100

    uncomment for install testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,46 +20,46 @@ brewfile () {
 
 homebrew () {
   echo homebrew;
-  # # install homebrew
-  # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  #
-  # # disable analytics
-  # brew analytics off
+  # install homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  # disable analytics
+  brew analytics off
 }
 
 neovim () {
   echo neovim;
-  # git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-  # # install neovim python module
-  # pip3 install neovim
+  git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+  # install neovim python module
+  pip3 install neovim
 }
 
 nvm () {
   echo nvm;
-  # # install node version manager
-  # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  # install node version manager
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 }
 
 symlinks () {
   echo symlinks;
-  # touch ~/.hushlogin
-  # touch ~/.aliases.priv
-  # mkdir -p ~/.config/nvim/lua/user
-  #
-  # ln -sf "$SCRIPT_DIR/aliases"      ~/.aliases
-  # ln -sf "$SCRIPT_DIR/gitconfig"    ~/.gitconfig
-  # ln -sf "$SCRIPT_DIR/init.lua"     ~/.config/nvim/lua/user/init.lua
-  # ln -sf "$SCRIPT_DIR/lockfile.lua" ~/.config/nvim/lua/lazy_snapshot.lua
-  # ln -sf "$SCRIPT_DIR/tmux.conf"    ~/.tmux.conf
-  # ln -sf "$SCRIPT_DIR/zimrc"        ~/.zimrc
-  # ln -sf "$SCRIPT_DIR/zshrc"        ~/.zshrc
+  touch ~/.hushlogin
+  touch ~/.aliases.priv
+  mkdir -p ~/.config/nvim/lua/user
+
+  ln -sf "$SCRIPT_DIR/aliases"      ~/.aliases
+  ln -sf "$SCRIPT_DIR/gitconfig"    ~/.gitconfig
+  ln -sf "$SCRIPT_DIR/init.lua"     ~/.config/nvim/lua/user/init.lua
+  ln -sf "$SCRIPT_DIR/lockfile.lua" ~/.config/nvim/lua/lazy_snapshot.lua
+  ln -sf "$SCRIPT_DIR/tmux.conf"    ~/.tmux.conf
+  ln -sf "$SCRIPT_DIR/zimrc"        ~/.zimrc
+  ln -sf "$SCRIPT_DIR/zshrc"        ~/.zshrc
 }
 
 shell () {
   echo shell;
-  # # use brew zsh instead of macos default zsh
-  # which zsh | sudo tee -a /etc/shells
-  # chsh -s "$(which zsh)"
+  # use brew zsh instead of macos default zsh
+  which zsh | sudo tee -a /etc/shells
+  chsh -s "$(which zsh)"
 }
 
 case "$1" in
